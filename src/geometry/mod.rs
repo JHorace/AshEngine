bce0e07c0a79aeeafdaa7e33d3ad5ee49c577ddc
JHorace <jasumihiro@gmail.com>
2021-01-25 17:02:10 -0800

    Added a scene structure that manages mesh instances Changed to static lib

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,3 +1,4 @@
 pub mod vertex;
 pub mod model;
 pub mod mesh;
+pub mod scene;
---
+++ src/geometry/scene/mod.rs
@@ -0,0 +1,22 @@
+use cgmath::{Matrix4, Vector4, SquareMatrix};
+
+type MeshID = u64;
+type InstanceID = u64;
+
+pub struct Instance
+{
+    mesh_id_: MeshID,
+    transform_: Matrix4<f32>,
+
+}
+
+impl Instance
+{
+
+
+}
+
+struct Scene
+{
+
+}
