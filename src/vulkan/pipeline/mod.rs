e9322eb2da9df0bd702b883939898fdaa1e9a405
JHorace <jasumihiro@gmail.com>
2021-02-05 14:07:12 -0800

    Added proper arguments for reflection tests

--- src/vulkan/pipeline/mod.rs
+++ src/vulkan/pipeline/mod.rs
@@ -47,7 +47,7 @@ impl Pipeline
         let vertex_shader = unsafe { shader::Shader::from_glsl(device, vert_path, ash::vk::ShaderStageFlags::VERTEX) };
         let fragment_shader = unsafe { shader::Shader::from_glsl(device, frag_path, ash::vk::ShaderStageFlags::FRAGMENT) };
 
-        Pipeline::shader_reflection_test(vertex_shader);
+        Pipeline::shader_reflection_test(&vertex_shader);
 
         let shader_stages = [
             vertex_shader.build_pipeline_shader_stage_create_info(),
@@ -90,9 +90,9 @@ impl Pipeline
 
         let input_variables = shader_module.enumerate_input_variables(None).unwrap();
         let output_variables = shader_module.enumerate_output_variables(None).unwrap();
-        let descriptor_sets = shader_module.enumerate_descriptor_sets().unwrap();
-        let bindings = shader_module.enumerate_descriptor_bindings();
-        let push_constants = shader_module.enumerate_push_constant_blocks();
+        let descriptor_sets = shader_module.enumerate_descriptor_sets(None).unwrap();
+        let bindings = shader_module.enumerate_descriptor_bindings(None);
+        let push_constants = shader_module.enumerate_push_constant_blocks(None);
 
     }
 
